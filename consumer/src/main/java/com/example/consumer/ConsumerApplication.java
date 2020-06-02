dded1c3324e68e82273daf4beeca8bb440ffa7df
zhangzhangzhang0 <794353281@qq.com>
2020-06-02 11:36:23 +0800

    5.28作业

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -3,9 +3,13 @@ package com.example.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- consumer/src/main/java/com/example/consumer/Controller.java
+++ consumer/src/main/java/com/example/consumer/Controller.java
@@ -1,5 +1,6 @@
 package com.example.consumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,27 @@ public class Controller {
     public RestTemplate restTemplate(){
         return  new RestTemplate();
     }
-    @Autowired
-    private RestTemplate restTemplate;
+
+    @Autowired(required = false)
+    private FeignClientService feignClientService;
+
+ //   @Autowired
+   // private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "executeHystrixHandle")
     @RequestMapping("/api/test/haha/get")
     public String consumer(){
-        return this.restTemplate.getForObject("http://localhost:6110/producer/haha", String.class);
+       // return this.restTemplate.getForObject("http://localhost:6110/producer/haha", String.class);
+        return feignClientService.consumer();
     }
+
+    //服务进入保护时，回调方法
+
+    public String executeHystrixHandle() {
+        return  "Hello, the current system has a large number of people, please try again later, please forgive me for the inconvenience! ! !";
+
+    }
+
+
 }
 
